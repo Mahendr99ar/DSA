8b7f40fd8cf43607d73d46753846c9410426a4c4
Mahendra Meena <mani06.damor@gmail.com>
2026-08-01 02:50:53 +0530

    Time: 3 ms (56.08%) | Memory: 9.5 MB (89.04%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -3,21 +3,22 @@ public:
     string multiply(string num1, string num2) {
         if (num1 == "0" || num2 == "0") return "0";
         
-        vector<int> res(num1.size()+num2.size(), 0);
+        vector<int> num(num1.size() + num2.size(), 0);
         
-        for (int i = num1.size()-1; i >= 0; i--) {
-            for (int j = num2.size()-1; j >= 0; j--) {
-                res[i + j + 1] += (num1[i]-'0') * (num2[j]-'0');
-                res[i + j] += res[i + j + 1] / 10;
-                res[i + j + 1] %= 10;
+        for (int i = num1.size() - 1; i >= 0; --i) {
+            for (int j = num2.size() - 1; j >= 0; --j) {
+                num[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+                num[i + j] += num[i + j + 1] / 10;
+                num[i + j + 1] %= 10;
             }
         }
         
         int i = 0;
-        string ans = "";
-        while (res[i] == 0) i++;
-        while (i < res.size()) ans += to_string(res[i++]);
+        while (i < num.size() && num[i] == 0) ++i;
         
-        return ans;
+        string res = "";
+        while (i < num.size()) res.push_back(num[i++] + '0');
+        
+        return res;
     }
 };
